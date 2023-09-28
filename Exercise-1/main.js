4b711514425c8e2a5d56c1299cd450d5ac3e8c4a
Tggooch123 <100807947+Tggooch123@users.noreply.github.com>
2023-09-28 12:47:00 -0500

    Uploaded to Github

---
+++ Exercise-1/main.js
@@ -0,0 +1,8 @@
+//String Type
+var studentName;
+studentName = "Ty Gooch";
+//Number Type
+var totalCourses;
+totalCourses = 1;
+//Console Log
+console.log(studentName, totalCourses);
---
+++ Exercise-1/main.ts
@@ -0,0 +1,14 @@
+//String Type
+// let studentName: string;
+
+// studentName = "Ty Gooch";
+
+
+//Number Type
+// let totalCourses: number;
+
+// totalCourses = 1
+
+
+//Console Log
+// console.log(studentName, totalCourses);
