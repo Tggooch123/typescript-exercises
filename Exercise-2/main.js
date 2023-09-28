f43f0549e60c20374e93e0a3a10dd354f07dfb22
Tggooch123 <100807947+Tggooch123@users.noreply.github.com>
2023-09-28 13:21:31 -0500

    Exercise-2 Completed

---
+++ Exercise-2/main.js
@@ -0,0 +1,10 @@
+// An array containing strings type
+var subjects;
+// An Object containing string and number type
+var student;
+student = {
+    name: "Ty Gooch",
+    age: 19,
+    subjects: "Front-End Development"
+};
+console.log(student);
---
+++ Exercise-2/main.ts
@@ -0,0 +1,17 @@
+// An array containing strings type
+//let subjects: string[];
+
+// An Object containing string and number type
+// let student: {
+    // name: string;
+   // age: number;
+   // subjects: string;
+//};
+
+// student = {
+   // name: "Ty Gooch",
+   // age: 19,
+   // subjects: "Front-End Development"
+//};
+
+// console.log(student);
